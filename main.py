586bdf2cf2690dbea6a8586a7050de1b2672bf79
gianluca236 <gianluca236@gmail.com>
2025-10-28 11:49:46 +0100

    programma finito

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import flet as ft
+
+import automobile
 from alert import AlertManager
 from autonoleggio import Autonoleggio
 
@@ -36,7 +38,26 @@ def main(page: ft.Page):
     lista_auto = ft.ListView(expand=True, spacing=5, padding=10, auto_scroll=True)
 
     # Tutti i TextField per le info necessarie per aggiungere una nuova automobile (marca, modello, anno, contatore posti)
-    # TODO
+
+    in_marca = ft.TextField( label="Marca"  )
+    in_modello = ft.TextField(label="Modello")
+    in_anno = ft.TextField( label="Anno")
+
+    count= ft.Text('0')
+    count.value=0
+
+    def incrementa(e):
+        count.value = str(int(count.value) + 1)
+        page.update()
+
+    def decrementa(e):
+        if count.value > '0':
+            count.value = str(int(count.value) - 1)
+            page.update()
+
+    riga= ft.Row([in_marca, in_modello, in_anno , ft.IconButton(ft.Icons.REMOVE, on_click=decrementa , icon_color='red'),
+                  count, ft.IconButton(ft.Icons.ADD, on_click=incrementa , icon_color='green')])
+
 
     # --- FUNZIONI APP ---
     def aggiorna_lista_auto():
@@ -57,15 +78,44 @@ def main(page: ft.Page):
         txt_responsabile.value = f"Responsabile: {autonoleggio.responsabile}"
         page.update()
 
-    # Handlers per la gestione dei bottoni utili all'inserimento di una nuova auto
-    # TODO
+
+    def clicked_button(e):
+
+
+        if in_anno.value.isalpha():
+            alert.show_alert('Inserire valore numerico per anno')
+
+        elif in_marca.value == "" :
+            alert.show_alert('Inserire almeno un valore per marca')
+
+        elif in_modello.value == "":
+            alert.show_alert('Inserire almeno un valore per modello')
+
+        elif in_anno.value == "":
+            alert.show_alert('Inserire almeno un valore per anno')
+
+
+
+        else:
+
+            autonoleggio.aggiungi_automobile(str(in_marca.value), str(in_modello.value),
+                                             in_anno.value, int(count.value))
+            aggiorna_lista_auto()
+
+            in_marca.value = ""
+            in_modello.value = ""
+            in_anno.value = ""
+
+            page.update()
+
+
+    btn = ft.ElevatedButton("Aggiungi auto", on_click=clicked_button)
 
     # --- EVENTI ---
     toggle_cambia_tema = ft.Switch(label="Tema scuro", value=True, on_change=cambia_tema)
     pulsante_conferma_responsabile = ft.ElevatedButton("Conferma", on_click=conferma_responsabile)
 
-    # Bottoni per la gestione dell'inserimento di una nuova auto
-    # TODO
+
 
     # --- LAYOUT ---
     page.add(
@@ -83,7 +133,10 @@ def main(page: ft.Page):
                alignment=ft.MainAxisAlignment.CENTER),
 
         # Sezione 3
-        # TODO
+
+        ft.Text("Aggiungi automobile", size=20),
+        riga,
+        btn,
 
         # Sezione 4
         ft.Divider(),
